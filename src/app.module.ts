a142fb9de27f29278d9dfa911a199c5e4cfc7271
Dmitro Serdun <d.serdun@webtrit.com>
2024-06-02 15:37:49 +0300

    feat: add todo api

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoModule } from './todo/todo.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), TodoModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
